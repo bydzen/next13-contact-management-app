0f35cb879b2b6a567a6b3db9644d08f3baba16b1
bydzen <svnteentthones@gmail.com>
2023-06-18 08:45:00 +0700

    add taglines

--- components/header.tsx
+++ components/header.tsx
@@ -2,6 +2,7 @@ export function Header() {
   return (
     <header>
       <h1 className="text-center font-bold text-3xl">Contact Management</h1>
+      <p className="text-center text-gray-500 pt-1">Record and organize your contact list simply and easily</p>
     </header>
   )
 }
