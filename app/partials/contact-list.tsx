b8a961ae3ee2e14715ac7b9dfc9f580e8fb20a96
bydzen <svnteentthones@gmail.com>
2023-06-18 09:05:36 +0700

    display msg if no contact

--- app/partials/contact-list.tsx
+++ app/partials/contact-list.tsx
@@ -8,6 +8,11 @@ interface ContactListProps {
 export default function ContactList({ contacts }: ContactListProps) {
   return (
     <ul className="space-y-4">
+      {contacts.length == 0 ? (
+        <p className="text-center py-7 text-gray-500">
+          No one contact here, try add your first contact.
+        </p>
+      ) : null}
       {contacts?.map((contact) => (
         <ContactItem contact={contact} />
       ))}
