f2bfbf0bf883369c7d1d12ff454b58476eda61ac
bydzen <svnteentthones@gmail.com>
2023-06-19 09:01:16 +0700

    error msg when db service not start or connected

--- app/partials/contact-list.tsx
+++ app/partials/contact-list.tsx
@@ -8,9 +8,14 @@ interface ContactListProps {
 export default function ContactList({ contacts }: ContactListProps) {
   return (
     <ul className="space-y-4">
-      {contacts?.length == 0 || contacts?.length == null ? (
+      {contacts?.length == 0 ? (
         <p className="text-center py-7 text-gray-500">
           No one contact here, try add your first contact.
+        </p>
+      ) : null}
+      {contacts?.length == null ? (
+        <p className="text-center py-7 text-red-500">
+          Failed to read database, check again or restart the server.
         </p>
       ) : null}
       {contacts?.map((contact) => (
